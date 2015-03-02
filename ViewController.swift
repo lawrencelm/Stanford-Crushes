8006093e34e7483a730efa979ea26844d8a9455a
lawrencelm <lmurata@stanford.edu>
2015-03-02 01:59:42 -0800

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stanford-Crushes
+//
+//  Created by Lawrence Lin Murata on 3/2/15.
+//  Copyright (c) 2015 Lawrence Lin Murata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
