140c05225414a51ee5f825e53f6fa628da72ddf7
lawrencelm <lmurata@stanford.edu>
2015-03-02 23:23:29 -0800

    testing how to hide info

--- File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  Stanford-Crushes
-//
-//  Created by Lawrence Lin Murata on 3/2/15.
-//  Copyright (c) 2015 Lawrence Lin Murata. All rights reserved.
-//
-
-import Foundation
