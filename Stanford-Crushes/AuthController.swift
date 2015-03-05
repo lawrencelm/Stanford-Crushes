bc32cb5ad063aceb079fa11f5f911fe3d79714d1
lawrencelm <lmurata@stanford.edu>
2015-03-04 19:22:10 -0800

    done implementing touch ID

---
+++ Stanford-Crushes/AuthController.swift
@@ -0,0 +1,108 @@
+//
+//  AuthController.swift
+//  Stanford-Crushes
+//
+//  Created by Lawrence Lin Murata on 3/4/15.
+//  Copyright (c) 2015 Lawrence Lin Murata. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class AuthController: UITabBarController, UIAlertViewDelegate
+ {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        authenticateUser()
+    }
+    
+    func showPasswordAlert() {
+        var passwordAlert : UIAlertView = UIAlertView(title: "TouchIDDemo", message: "Please type your password", delegate: self, cancelButtonTitle: "Cancel", otherButtonTitles: "Okay")
+        passwordAlert.alertViewStyle = UIAlertViewStyle.SecureTextInput
+        passwordAlert.show()
+    }
+    
+    func alertView(alertView: UIAlertView!, clickedButtonAtIndex buttonIndex: Int) {
+        if buttonIndex == 1 {
+            if !alertView.textFieldAtIndex(0)!.text.isEmpty {
+                if alertView.textFieldAtIndex(0)!.text == "appcoda" {
+                    
+                }
+                else{
+                    showPasswordAlert()
+                }
+            }
+            else{
+                showPasswordAlert()
+            }
+        }
+    }
+    
+    func authenticateUser() {
+        // Get the local authentication context.
+        let context = LAContext()
+        
+        // Declare a NSError variable.
+        var error: NSError?
+        
+        // Set the reason string that will appear on the authentication alert.
+        var reasonString = "Authentication is needed to access your notes."
+        
+        // Check if the device can evaluate the policy.
+        if context.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
+            [context .evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString, reply: { (success: Bool, evalPolicyError: NSError?) -> Void in
+                
+                if success {
+                    
+                }
+                else{
+                    // If authentication failed then show a message to the console with a short description.
+                    // In case that the error is a user fallback, then show the password alert view.
+                    println(evalPolicyError?.localizedDescription)
+                    
+                    switch evalPolicyError!.code {
+                        
+                    case LAError.SystemCancel.rawValue:
+                        println("Authentication was cancelled by the system")
+                        
+                    case LAError.UserCancel.rawValue:
+                        println("Authentication was cancelled by the user")
+                        
+                    case LAError.UserFallback.rawValue:
+                        println("User selected to enter custom password")
+                        self.showPasswordAlert()
+                        
+                    default:
+                        println("Authentication failed")
+                        self.showPasswordAlert()
+                    }
+                }
+                
+            })]
+        }
+        else{
+            // If the security policy cannot be evaluated then show a short message depending on the error.
+            switch error!.code{
+                
+            case LAError.TouchIDNotEnrolled.rawValue:
+                println("TouchID is not enrolled")
+                
+            case LAError.PasscodeNotSet.rawValue:
+                println("A passcode has not been set")
+                
+            default:
+                // The LAError.TouchIDNotAvailable case.
+                println("TouchID not available")
+            }
+            
+            // Optionally the error description can be displayed on the console.
+            println(error?.localizedDescription)
+            
+            // Show the custom alert view to allow users to enter the password.
+            self.showPasswordAlert()
+        }
+    }
+}
---
+++ Stanford-Crushes/ChatCollectionView.swift
@@ -0,0 +1,9 @@
+//
+//  ChatCollectionView.swift
+//  Stanford-Crushes
+//
+//  Created by Lawrence Lin Murata on 3/4/15.
+//  Copyright (c) 2015 Lawrence Lin Murata. All rights reserved.
+//
+
+import Foundation
