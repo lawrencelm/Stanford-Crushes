ee6c8cfc64cf0b5b36ff6cf0762607891397a6a2
lawrencelm <lmurata@stanford.edu>
2015-03-04 20:02:54 -0800

    finished up UI for 2nd feature

--- Stanford-Crushes/MatchViewController.swift
+++ Stanford-Crushes/MatchViewController.swift
@@ -13,12 +13,18 @@ class MatchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        //image that matches #E74C3C theme
+        let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0.0, y: 0.0), size: view.frame.size))
+        imageView.image = UIImage(named: "pic1.jpg")
+        message.superview?.addSubview(imageView)
+        message.superview?.sendSubviewToBack(imageView)    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var message: UILabel!
     
     @IBAction func enteredCrush(sender: UITextField) {
         //limit to 5
