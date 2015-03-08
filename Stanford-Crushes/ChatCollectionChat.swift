d8bea1440531286c74ea580c04657913f4303acc
lawrencelm <lmurata@stanford.edu>
2015-03-07 19:23:55 -0800

    implementing UICollectionView

---
+++ Stanford-Crushes/ChatCollectionChat.swift
@@ -0,0 +1,9 @@
+//
+//  ChatCollectionChat.swift
+//  Stanford-Crushes
+//
+//  Created by Lawrence Lin Murata on 3/7/15.
+//  Copyright (c) 2015 Lawrence Lin Murata. All rights reserved.
+//
+
+import Foundation
