16fcf5c9f13a09cb1beacf36723cd70b45a6fa53
lawrencelm <lmurata@stanford.edu>
2015-03-07 22:08:16 -0800

    finished auto resize feature

--- Stanford-Crushes/ChatCollectionView.swift
+++ Stanford-Crushes/ChatCollectionView.swift
@@ -69,7 +69,8 @@ class ChatViewController : UICollectionViewController, UICollectionViewDelegateF
             var width: CGFloat = 100
             var height: CGFloat = 100
             
-            var multiply = 1
+            var multiplyWidth = 1
+            var multiplyHeader = 1
             
            // var convo : Array<String>?
             var index = Int()
@@ -87,8 +88,13 @@ class ChatViewController : UICollectionViewController, UICollectionViewDelegateF
             var query = PFQuery(className: classType)
             var array = query.findObjects()
             let factor = 5
-            let margin = 10
+            let margin = 20
             let maxWidth = UIScreen.mainScreen().bounds.size.width - 2*CGFloat(margin)
+            
+            //let otherMaxWidth = self.view.bounds.size.width
+            
+            //println(UIScreen.mainScreen().bounds.size.width)
+            //println(self.view.bounds.size.width)
             
             if (array != nil) {
                 println("ARRAY NOT NIL")
@@ -101,9 +107,12 @@ class ChatViewController : UICollectionViewController, UICollectionViewDelegateF
                // convo = array[index].objectForKey("conversation") as Array
                         println("MULTIPLYING CONVO")
                         if convo!.count > factor {
-                            multiply *= convo!.count / factor
-                            if CGFloat(multiply) >= maxWidth{
-                                multiply = 1
+                            multiplyHeader *= convo!.count / factor
+                            multiplyWidth *= convo!.count / factor
+
+                            if width*CGFloat(multiplyWidth) >= maxWidth{
+                                println("RESIZING")
+                                multiplyWidth = 1
                                 width = maxWidth
                             }
                         }
@@ -111,7 +120,7 @@ class ChatViewController : UICollectionViewController, UICollectionViewDelegateF
                 }
             }
             
-            return CGSize(width: width*CGFloat(multiply), height: height*CGFloat(multiply))
+            return CGSize(width: width*CGFloat(multiplyWidth), height: height*CGFloat(multiplyHeader))
     }
     
     //3
