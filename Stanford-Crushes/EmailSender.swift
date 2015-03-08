85fbf48e8b0758035ba6045979526ced7ba12180
lawrencelm <lmurata@stanford.edu>
2015-03-08 15:10:48 -0700

    implemented email functionality for anon crushes

---
+++ Stanford-Crushes/EmailSender.swift
@@ -0,0 +1,13 @@
+//
+//  EmailSender.swift
+//  Stanford-Crushes
+//
+//  Created by Lawrence Lin Murata on 3/8/15.
+//  Copyright (c) 2015 Lawrence Lin Murata. All rights reserved.
+//
+
+import MessageUI
+
+class EmailSender: UIViewController, MFMailComposeViewControllerDelegate, UITextFieldDelegate, UITextViewDelegate {
+    
+}
--- Stanford-Crushes/WriteViewController.swift
+++ Stanford-Crushes/WriteViewController.swift
@@ -5,10 +5,23 @@
 //  Created by Lawrence Lin Murata on 3/2/15.
 //  Copyright (c) 2015 Lawrence Lin Murata. All rights reserved.
 //
+//  Detail: The problem with MFMailComposeViewController
+//  is that there is no way to set the "from" field in the e-mail,
+//  so it's only going to be the e-mail account that is connected
+//  to the phone. Ideally, the e-mail feature would be 100%
+//  anonymous and send an e-mail from a "Stanford Crushes" account.
+//  That would require a standalone e-mail client. I chose to
+//  use a solution that relies 100% only on the iOS SDK for
+//  the purposes of my project since it's a major feature not
+//  covered in lecture. I plan to change it to make
+//  it more anonymous before I upload it
+//  into the App Store.
 
 import UIKit
+import MessageUI
+import Foundation
 
-class WriteViewController: UIViewController, UITextFieldDelegate
+class WriteViewController: UIViewController, MFMailComposeViewControllerDelegate, UITextFieldDelegate, UITextViewDelegate
 {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         var destination = segue.destinationViewController as? UIViewController
@@ -17,13 +30,14 @@ class WriteViewController: UIViewController, UITextFieldDelegate
         }
         if let mtvc = destination as? CrushesTableViewController {
             //means that clicked post
-            if postText != nil {
+            if postText != nil && postText.text != "" {
                 var post = PFObject(className: "AnonCrush")
                 post.setObject(postText.text, forKey: "post")
                 
-                if email != nil {
+                if email != nil && email.text != "" {
                     post.setObject(email.text, forKey: "crush_email")
                     post.setObject(true, forKey: "set_name")
+                    sendMail()
                 } else {
                     post.setObject(false, forKey: "set_name")
                 }
@@ -49,18 +63,13 @@ class WriteViewController: UIViewController, UITextFieldDelegate
     @IBOutlet weak var email: UITextField!
     // MARK: - Public API
     
-   /* var postText: String? {
-        didSet {
-            searchTextField?.text = postText
-            //refresh()
-        }
-    }*/
-    
     // MARK: - View Controller Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //postText.delegate = self
+        //email.delegate = self
         self.parentViewController?.title = "Top posts";
         var rightBarButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Compose, target:self, action: "composeButton")
         self.parentViewController?.navigationItem.rightBarButtonItem = rightBarButton;
@@ -68,6 +77,38 @@ class WriteViewController: UIViewController, UITextFieldDelegate
         rightBarButton.target = self
         refresh()
         
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients([email.text])
+        mailComposerVC.setSubject("Somebody has a crush on you!")
+        mailComposerVC.setMessageBody(postText.text, isHTML: true)
+        
+        return mailComposerVC
+    }
+    
+    func sendMail() {
+        var mailComposeViewController = configuredMailComposeViewController()
+
+        if MFMailComposeViewController.canSendMail() {
+            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func unwindToRecent(segue: UIStoryboardSegue) {
