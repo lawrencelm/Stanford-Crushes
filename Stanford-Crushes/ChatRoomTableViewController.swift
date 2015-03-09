a98c1595339590029332a46845214ab676d395ba
lawrencelm <lmurata@stanford.edu>
2015-03-08 22:02:28 -0700

    finally done with Chat Room UI

--- Stanford-Crushes/ChatRoomTableViewController.swift
+++ Stanford-Crushes/ChatRoomTableViewController.swift
@@ -13,10 +13,45 @@ import Foundation
 class ChatRoomTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("reloadTable"), userInfo: nil, repeats: true)
     }
     
-    func update() {
+    func reloadTable() {
         println("update")
+        tableView.reloadData()
     }
+    
+    // MARK: - Storyboard Connectivity
+    
+    
+    private struct Storyboard {
+        static let CellReuseIdentifier = "convo"
+    }
+    
+    // MARK: - Table View Data Source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var query = PFQuery(className: "AnonCrush")
+        var array = query.findObjects()
+        if array.count < 100 {
+            return array.count
+        } else {
+            return 100
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.CellReuseIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        //cell.row = indexPath.row
+        
+        return cell
+    }
+    
 }
